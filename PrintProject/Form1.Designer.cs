34e9050e92e498db9aae9f0b609c5d0f50f1d352
AugustLC <75476801+AugustLC@users.noreply.github.com>
2022-09-03 23:18:56 +0300

    Add files via upload

---
+++ PrintProject/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PrintProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(431, 423);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Печать бирок";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PrintProject/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Printing;
+
+using ceTe.DynamicPDF;
+using ceTe.DynamicPDF.Rasterizer;
+
+using RawPrint;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace PrintProject
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Info.columns = 2;
+            Info.width = 220;
+            Info.height = 220;
+            //Info.columns = (int)numericUpDown1.Value;
+            string[] hhh = { };
+            Info.allfiles = hhh;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                int num1 = e.Location.Y / Info.height;
+
+                int num2 = e.Location.X / Info.width;
+
+                string Filepath = Info.pdfs[num1*Info.columns + num2];
+                
+
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.Verb = "print";
+                info.FileName = Filepath;
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+
+                p.WaitForInputIdle();
+                System.Threading.Thread.Sleep(2500);
+                if (false == p.CloseMainWindow())
+                    p.Kill();
+                    
+            }
+            catch { }
+        }
+
+
+        /*private void update()
+        {
+            Info.columns = (int)numericUpDown1.Value;
+
+            Info.pics = new List<Bitmap>();
+
+            GC.Collect();
+
+            Thread.Sleep(100);
+
+            DirectoryInfo dir = new DirectoryInfo("Temp");
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                f.Delete();
+            }
+
+            string[] allPdf = Directory.GetFiles("PDFs");
+            Info.pdfs = new List<string>();
+            Info.pics = new List<Bitmap>();
+            Info.picPaths = new List<string>();
+            pictureBox1.Height = 0;
+
+
+            foreach (string filename in allPdf)
+            {
+                PdfRasterizer rasterizer = new PdfRasterizer(filename);
+                rasterizer.Draw("Temp\\" + Path.GetFileNameWithoutExtension(filename) + ".png", ImageFormat.Png, ImageSize.Dpi300);
+                Info.pdfs.Add(filename);
+            }
+
+            pictureBox1.Image = new Bitmap(2500, 10000);
+            Graphics graph = Graphics.FromImage(pictureBox1.Image);
+
+            int i = 0, j = 0;
+
+            string[] allfiles = Directory.GetFiles("Temp");
+
+            foreach (string filename in allfiles)
+            {
+                Info.pics.Add(new Bitmap(filename));
+                float tempScale = Info.pics[Info.pics.Count - 1].Height / 100;
+                tempScale = Info.pics[Info.pics.Count - 1].Width / tempScale;
+                Info.width = (int)tempScale + 20;
+                Info.pics[Info.pics.Count - 1] = new Bitmap(Info.pics[Info.pics.Count - 1], new Size((int)tempScale, 100));
+                Info.picPaths.Add(filename);
+
+                //if (Info.pics[Info.pics.Count - 1].Width > width) width = Info.pics[Info.pics.Count - 1].Width;
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+                if (i >= Info.columns) { i = 0; j++; pictureBox1.Height = pictureBox1.Height + Info.height; }
+
+                graph.DrawImage(Info.pics[Info.pics.Count - 1], new Point(i * Info.width, j * Info.height));
+
+                i++;
+
+
+
+            }
+
+            if (i < Info.columns) pictureBox1.Height = pictureBox1.Height + Info.height;
+
+            int width = Info.width * Info.columns;
+            pictureBox1.Width = width;
+
+            //panel1.Width = width + 20;
+            //this.Width = width + 50;
+        }*/
+
+
+        private void update()
+        {
+            
+
+            Info.pics = new List<Bitmap>();
+
+            GC.Collect();
+
+            Thread.Sleep(500);
+
+            DirectoryInfo dir = new DirectoryInfo("Temp");
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                f.Delete();
+            }
+
+            string[] allPdf = Directory.GetFiles("PDFs");
+            Info.pdfs = new List<string>();
+            Info.pics = new List<Bitmap>();
+            Info.picPaths = new List<string>();
+            pictureBox1.Height = 0;
+
+
+            foreach (string filename in allPdf)
+            {
+                PdfRasterizer rasterizer = new PdfRasterizer(filename);
+                rasterizer.Draw("Temp\\" + Path.GetFileNameWithoutExtension(filename) + ".png", ImageFormat.Png, ImageSize.Dpi300);
+                Info.pdfs.Add(filename);
+            }
+
+            pictureBox1.Image = new Bitmap(2500, 10000);
+            Graphics graph = Graphics.FromImage(pictureBox1.Image);
+
+            int i = 0, j = 0, k=0;
+
+            string[] allfiles = Directory.GetFiles("Temp");
+            Info.allfiles = allfiles;
+
+            foreach (string filename in allfiles)
+            {
+                Info.pics.Add(new Bitmap(filename));
+                float tempScale = Info.pics[Info.pics.Count - 1].Height / 200;
+                tempScale = Info.pics[Info.pics.Count - 1].Width / tempScale;
+                if(k==0) Info.width = (int)tempScale + 20;
+                Info.pics[Info.pics.Count - 1] = new Bitmap(Info.pics[Info.pics.Count - 1], new Size((int)tempScale, 200));
+                Info.picPaths.Add(filename);
+
+                //if (Info.pics[Info.pics.Count - 1].Width > width) width = Info.pics[Info.pics.Count - 1].Width;
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+                if (i >= Info.columns) { i = 0; j++; pictureBox1.Height = pictureBox1.Height + Info.height; }
+
+                graph.DrawImage(Info.pics[Info.pics.Count - 1], new Point(i * Info.width, j * Info.height));
+
+                i++;
+
+                k++;
+
+            }
+
+            if (i <= Info.columns) pictureBox1.Height = pictureBox1.Height + Info.height;
+
+            int width = Info.width * Info.columns;
+            pictureBox1.Width = width;
+
+            
+
+            //panel1.Width = width + 20;
+            //this.Width = width + 50;
+        }
+
+
+
+        private void update_pics()
+        {
+            //Info.columns = (int)numericUpDown1.Value;
+
+            pictureBox1.Image = new Bitmap(2500, 10000);
+            Graphics graph = Graphics.FromImage(pictureBox1.Image);
+
+            int i = 0, j = 0, k = 0;
+
+            pictureBox1.Height = 150;
+
+            string[] allfiles = Info.allfiles;
+
+            if (allfiles.Length != 0)
+            foreach (string filename in allfiles)
+            {
+                //Info.pics.Add(new Bitmap(filename));
+                //float tempScale = Info.pics[Info.pics.Count - 1].Height / 100;
+                //tempScale = Info.pics[Info.pics.Count - 1].Width / tempScale;
+                //Info.width = (int)tempScale + 20;
+                //Info.pics[Info.pics.Count - 1] = new Bitmap(Info.pics[Info.pics.Count - 1], new Size((int)tempScale, 100));
+                //Info.picPaths.Add(filename);
+
+                //if (Info.pics[Info.pics.Count - 1].Width > width) width = Info.pics[Info.pics.Count - 1].Width;
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+
+                //Info.height = Info.pics[Info.pics.Count - 1].Height;
+
+                if (i >= Info.columns) { i = 0; j++; pictureBox1.Height = pictureBox1.Height + Info.height; }
+
+                graph.DrawImage(Info.pics[k], new Point(i * Info.width, j * Info.height));
+
+                i++;
+                k++;
+            }
+
+            if (i <= Info.columns) pictureBox1.Height = pictureBox1.Height + Info.height;
+
+            int width = Info.width * Info.columns;
+            pictureBox1.Width = width;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            update();
+            Info.columns = (int)panel1.Size.Width / Info.width;
+            update_pics();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Info.pics = new List<Bitmap>();
+
+            GC.Collect();
+
+            Thread.Sleep(500);
+
+            DirectoryInfo dir = new DirectoryInfo("Temp");
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                f.Delete();
+            }
+        }
+
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            //numericUpDown1.Value = (int)panel1.Size.Width / Info.width;
+            Info.columns = (int)panel1.Size.Width / Info.width;
+            update_pics();
+        }
+    }
+
+    public class Info
+    {
+        public static List<string> pdfs;
+        public static List<Bitmap> pics;
+        public static List<string> picPaths;
+        public static int width;
+        public static int height;
+        public static int columns;
+        public static string[] allfiles;
+    }
+}
